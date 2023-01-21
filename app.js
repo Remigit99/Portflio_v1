a7b804b3ae7189fc5d3e1234ba4323462ef2dda2
Aderemi Abiodun <104827360+Remigit99@users.noreply.github.com>
2023-01-21 01:07:09 +0100

    Commit first version of portfolio using vanilla js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+  /*=========Initialize Swiper =======*/
+  // <script>
+  //    var swiper = new Swiper(".mySwiper", {
+  //      cssMode: true,
+  //      navigation: {
+  //        nextEl: ".swiper-button-next",
+  //        prevEl: ".swiper-button-prev",
+  //      },
+  //      pagination: {
+  //        el: ".swiper-pagination",
+  //      },
+  //      mousewheel: true,
+  //      keyboard: true,
+  //    });
+  //  </script>
+
+
+AOS.init({
+  duration: 1500
+});
+
